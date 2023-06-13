d8e54e2eb8442e9fcd7675535c3ab8718bff5696
Xewol <bartekdeluga0702@gmail.com>
2023-06-13 11:25:09 +0200

    feat: breadcrumbs

---
+++ src/components/BreadCrumbs/index.tsx
@@ -0,0 +1,29 @@
+import { faAngleRight, faHome } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+interface Crumbs {
+  path: string[];
+}
+
+const BreadCrumbs = ({ path }: Crumbs) => {
+  return (
+    <ul className="flex-shrink-0 flex mb-3 text-gray-500">
+      {path.map((crumb, idx) => (
+        <li className="flex items-center">
+          <button
+            className="hover:text-gray-300 capitalize disabled:pointer-events-none flex items-center gap-1 font-bold"
+            disabled={idx === path.length - 1}
+          >
+            {idx === 0 && <FontAwesomeIcon className="text-xs" icon={faHome} />}
+            {crumb}
+          </button>
+          {idx < path.length - 1 && (
+            <FontAwesomeIcon icon={faAngleRight} className="mx-1.5" />
+          )}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default BreadCrumbs;
