ba35244d744e4d637c3b2446a39c2ee92b01706c
Xewol <bartekdeluga0702@gmail.com>
2023-06-15 17:28:11 +0200

    feat: added api response type

--- src/types.ts
+++ src/types.ts
@@ -12,6 +12,13 @@ export interface Book {
   canonical_isbn: string;
   authors: string[];
   page_count: number;
+  categories: string[];
   cover_art_url: string;
   published_works: Published_work[];
+}
+
+export interface ApiResponse {
+  total_results: number;
+  total_pages: number;
+  results: Book[];
 }
