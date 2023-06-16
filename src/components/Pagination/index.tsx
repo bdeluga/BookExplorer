9ed6800e1d7a47d3403c1142e99abd392bde1a72
Xewol <bartekdeluga0702@gmail.com>
2023-06-16 20:47:15 +0200

    refactor: pagination

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,32 +1,44 @@
 interface PaginationProps {
   page: number;
   setPage: (page: number) => void;
-  totalPages: number;
+  totalPages?: number;
+  isLoading: boolean;
 }
 
-const Pagination = ({ page, totalPages, setPage }: PaginationProps) => {
+const Pagination = ({
+  page,
+  totalPages,
+  setPage,
+  isLoading,
+}: PaginationProps) => {
+  if (!totalPages) return null;
+
   return (
-    <ul className="inline-flex -space-x-px mt-4">
+    <ul className="inline-flex -space-x-px mt-8 self-end mr-4">
       <button
-        disabled={page === 1}
+        disabled={isLoading || page === 1}
         onClick={() => setPage(page - 1)}
         className="pagination"
       >
         Previous
       </button>
       {page - 1 >= 1 && <button className="pagination">{page - 1}</button>}
-      <button className="pagination bg-gray-600 pointer-events-none">
+      <button className="pagination bg-gray-700 pointer-events-none">
         {page}
       </button>
-      <button className="pagination">
+      <button className="pagination" disabled={isLoading}>
         {page + 1 < totalPages - 1 ? page + 1 : page - 1}
       </button>
-      <button className="pagination">...</button>
+      <button className="pagination" disabled={isLoading}>
+        ...
+      </button>
 
-      <button className="pagination">{totalPages}</button>
+      <button className="pagination" disabled={isLoading}>
+        {totalPages}
+      </button>
 
       <button
-        disabled={page === totalPages}
+        disabled={isLoading || page === totalPages}
         onClick={() => setPage(page + 1)}
         className="pagination"
       >
