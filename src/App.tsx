e2a0d47cb14a7b7e0a85f01fbdabbb635f98111f
Xewol <bartekdeluga0702@gmail.com>
2023-06-16 20:49:22 +0200

    feat: minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,49 @@
 import { useState } from "react";
-import BreadCrumbs from "./components/BreadCrumbs";
-import Header from "./components/Header";
 import Table from "./components/Table";
 import { useBooks } from "./hooks/useBooks";
 import Pagination from "./components/Pagination";
-import { Categories } from "./types";
+
+import Nav from "./components/Nav";
+import BreadCrumbs from "./components/BreadCrumbs";
+import { Category } from "./types";
 
 export default function App() {
   const [page, setPage] = useState(1);
-  const [category, setCategory] = useState<Categories | undefined>();
-  const { data: books, isLoading } = useBooks(category, page);
+  const [category, setCategory] = useState<Category>();
+  const {
+    data: books,
+    isPreviousData,
+    error,
+    isLoading,
+  } = useBooks(category, page);
   const [crumbs, setCrumbs] = useState(["home"]);
 
+  // const updateCrumbs = (crumb: string) => {};
+  const changeCategory = (category: Category) => {
+    setCategory(category);
+    setPage(1);
+  };
+
   return (
-    <div className="flex min-h-screen flex-col text-gray-100 font-['Inter'] bg-gray-900">
-      <Header />
+    <div className="flex min-h-screen max-w-[100rem] mx-auto flex-col text-gray-100 font-['Inter'] ">
       <main className="flex-1 flex p-10 w-full ">
-        <div className="basis-1/4 sticky flex flex-col items-end pr-10 top-10 h-full">
+        <div className="basis-1/4 sticky top-10 h-full">
+          <Nav category={category} setCategory={changeCategory} />
           <BreadCrumbs path={crumbs} />
-          <h2 className="mb-8 lg:mb-3 font-semibold text-slate-900 dark:text-slate-200">
-            Categories
-          </h2>
-          <ul className="space-y-6 lg:space-y-2 border-l border-slate-100 dark:border-slate-800">
-            <li>
-              <button className="nav">Animals, Bugs & Pets</button>
-            </li>
-            <li>
-              <button className="nav">Art, Creativity & Music</button>
-            </li>
-            <li>
-              <button className="nav">General Literature</button>
-            </li>
-            <li>
-              <button className="nav">Hobbies, Sports & Outdoors</button>
-            </li>
-            <li>
-              <button className="nav">Science Fiction & Fantasy</button>
-            </li>
-            <li>
-              <button className="nav">Real Life</button>
-            </li>
-            <li>
-              <button className="nav">Science & Technology</button>
-            </li>
-            <li>
-              <button className="nav">Mystery & Suspense</button>
-            </li>
-            <li>
-              <button className="nav">Reference</button>
-            </li>
-          </ul>
         </div>
-        <div className="basis-3/4">
+        <div className="basis-full md:basis-3/4 px-20 flex flex-col ">
           <Table
             totalPages={books?.total_pages || 1}
-            headers={["ID", "Title", "Author(s)", "Kind(s)"]}
-            data={books?.results || []}
+            headers={["ISBN", "Title", "Author(s)", "Kind(s)"]}
+            data={books?.results}
+            error={error as object}
+            isLoading={isLoading}
           />
           <Pagination
             page={page}
             setPage={setPage}
-            totalPages={books?.total_pages || 1}
+            totalPages={books?.total_pages}
+            isLoading={isPreviousData}
           />
         </div>
       </main>
--- src/components/BreadCrumbs/index.tsx
+++ src/components/BreadCrumbs/index.tsx
@@ -7,22 +7,29 @@ interface Crumbs {
 
 const BreadCrumbs = ({ path }: Crumbs) => {
   return (
-    <ul className="flex-shrink-0 flex mb-3 ml-4 text-gray-500">
-      {path.map((crumb, idx) => (
-        <li className="flex items-center" key={crumb}>
-          <button
-            className="hover:text-gray-300 capitalize disabled:pointer-events-none flex items-center gap-1 font-bold"
-            disabled={idx === path.length - 1}
-          >
-            {idx === 0 && <FontAwesomeIcon className="text-xs" icon={faHome} />}
-            {crumb}
-          </button>
-          {idx < path.length - 1 && (
-            <FontAwesomeIcon icon={faAngleRight} className="mx-1.5" />
-          )}
-        </li>
-      ))}
-    </ul>
+    <>
+      <h2 className="mt-8 mb-2  font-semibold text-slate-900 dark:text-slate-200">
+        Location
+      </h2>
+      <ul className="flex-shrink-0 flex  text-gray-500">
+        {path.map((crumb, idx) => (
+          <li className="flex items-center" key={crumb}>
+            <button
+              className="hover:text-gray-300 capitalize disabled:pointer-events-none flex items-center gap-1 font-bold"
+              disabled={idx === path.length - 1}
+            >
+              {idx === 0 && (
+                <FontAwesomeIcon className="text-xs" icon={faHome} />
+              )}
+              {crumb}
+            </button>
+            {idx < path.length - 1 && (
+              <FontAwesomeIcon icon={faAngleRight} className="mx-1.5" />
+            )}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
