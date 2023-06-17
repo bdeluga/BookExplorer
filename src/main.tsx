0494b08d84e073e12c44b148d6084c4270c36e1d
Xewol <bartekdeluga0702@gmail.com>
2023-06-17 18:09:21 +0200

    refactor: moved header to app

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
-import Header from "./components/Header/index.tsx";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <Header />
       <App />
     </QueryClientProvider>
   </React.StrictMode>
