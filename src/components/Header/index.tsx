992505d26ad6885f3ae12f9068de23792a4a9511
Xewol <bartekdeluga0702@gmail.com>
2023-06-17 18:09:44 +0200

    refactor: header logo placement

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Header = () => {
   return (
-    <header className="border-b md:pl-40 justify-center md:justify-start h-16 border-gray-700 flex items-center text-2xl font-bold text-gray-100">
+    <header className="border-b lg:pl-40 justify-center lg:justify-start h-16 border-gray-700 flex items-center text-2xl font-bold text-gray-100">
       <div className="flex gap-2 items-center">
         {/* should be a link */}
         <span className=" from-orange-200 bg-gradient-to-r to-blue-400 text-transparent bg-clip-text">
