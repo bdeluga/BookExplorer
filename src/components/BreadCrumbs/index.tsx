a3e9b7ab97d5ee9f0641c28f1411912b775029ca
Xewol <bartekdeluga0702@gmail.com>
2023-06-17 18:08:28 +0200

    refactor: almost finished

--- src/components/BreadCrumbs/index.tsx
+++ src/components/BreadCrumbs/index.tsx
@@ -3,33 +3,35 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 interface Crumbs {
   path: string[];
+  homeFn: () => void;
 }
 
-const BreadCrumbs = ({ path }: Crumbs) => {
+const BreadCrumbs = ({ path, homeFn }: Crumbs) => {
   return (
-    <>
-      <h2 className="mt-8 mb-2  font-semibold text-slate-900 dark:text-slate-200">
-        Location
-      </h2>
-      <ul className="flex-shrink-0 flex  text-gray-500">
-        {path.map((crumb, idx) => (
-          <li className="flex items-center" key={crumb}>
-            <button
-              className="hover:text-gray-300 capitalize disabled:pointer-events-none flex items-center gap-1 font-bold"
-              disabled={idx === path.length - 1}
-            >
-              {idx === 0 && (
-                <FontAwesomeIcon className="text-xs" icon={faHome} />
-              )}
-              {crumb}
-            </button>
-            {idx < path.length - 1 && (
-              <FontAwesomeIcon icon={faAngleRight} className="mx-1.5" />
-            )}
-          </li>
-        ))}
-      </ul>
-    </>
+    <ul className="flex pl-4 mb-2 mt-4 md:mt-0 md:mb-4 text-gray-500 whitespace-nowrap text-xs md:text-base">
+      <li>
+        <button
+          className="hover:text-gray-300 capitalize disabled:pointer-events-none flex items-center gap-1 font-bold"
+          disabled={!path.length}
+          onClick={homeFn}
+        >
+          <FontAwesomeIcon icon={faHome} /> Home
+        </button>
+      </li>
+      {path.map((crumb, idx) => (
+        <li className="flex items-center" key={crumb}>
+          {idx < path.length && (
+            <FontAwesomeIcon icon={faAngleRight} className="mx-1.5" />
+          )}
+          <button
+            className="hover:text-gray-300 capitalize disabled:pointer-events-none font-bold"
+            disabled={idx === path.length - 1}
+          >
+            {crumb}
+          </button>
+        </li>
+      ))}
+    </ul>
   );
 };
 
