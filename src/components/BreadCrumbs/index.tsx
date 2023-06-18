5838b4382804f32c6329b1f3314483e3e9c959e7
Xewol <bartekdeluga0702@gmail.com>
2023-06-18 23:43:18 +0200

    feat: better BreadCrumbs

--- src/components/BreadCrumbs/index.tsx
+++ src/components/BreadCrumbs/index.tsx
@@ -1,34 +1,26 @@
-import { faAngleRight, faHome } from "@fortawesome/free-solid-svg-icons";
+import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Crumbs } from "../../types";
 
-interface Crumbs {
-  path: string[];
-  homeFn: () => void;
+interface Props {
+  crumbs: Crumbs[];
 }
 
-const BreadCrumbs = ({ path, homeFn }: Crumbs) => {
+const BreadCrumbs = ({ crumbs }: Props) => {
   return (
     <ul className="flex pl-4 mb-2 mt-4 md:mt-0 md:mb-4 text-gray-500 whitespace-nowrap text-xs md:text-base">
-      <li>
-        <button
-          className="hover:text-gray-300 capitalize disabled:pointer-events-none flex items-center gap-1 font-bold"
-          disabled={!path.length}
-          onClick={homeFn}
-        >
-          <FontAwesomeIcon icon={faHome} /> Home
-        </button>
-      </li>
-      {path.map((crumb, idx) => (
-        <li className="flex items-center" key={crumb}>
-          {idx < path.length && (
-            <FontAwesomeIcon icon={faAngleRight} className="mx-1.5" />
-          )}
+      {crumbs.map((crumb, idx) => (
+        <li key={crumb.label}>
           <button
             className="hover:text-gray-300 capitalize disabled:pointer-events-none font-bold"
-            disabled={idx === path.length - 1}
+            onClick={crumb.returnFn}
+            disabled={idx === crumbs.length - 1}
           >
-            {crumb}
+            {crumb.label}
           </button>
+          {idx < crumbs.length - 1 && (
+            <FontAwesomeIcon icon={faAngleRight} className="mx-1.5" />
+          )}
         </li>
       ))}
     </ul>
