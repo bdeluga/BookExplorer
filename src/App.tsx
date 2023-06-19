8bfa4c0c94fa3b87c03e3394103d49fe6d41fe96
Xewol <bartekdeluga0702@gmail.com>
2023-06-19 12:04:46 +0200

    refactor: nav

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,15 @@ import { useState } from "react";
 import Table from "./components/Table";
 import { useBooks } from "./hooks/useBooks";
 import Pagination from "./components/Pagination";
-
 import Nav from "./components/Nav";
-import type { Book, Crumbs, ColumnHeader, Kind } from "./types";
-
 import Header from "./components/Header";
-import Hamburger from "./components/Hamburger";
 import BreadCrumbs from "./components/BreadCrumbs";
+import type { Book, Crumbs, ColumnHeader, Kind } from "./types";
 
 export default function App() {
   const [page, setPage] = useState(1);
   const [kind, setKind] = useState<Kind>("General Literature");
   const [author, setAuthor] = useState("");
-  const [open, setOpen] = useState(false);
   const [selectedRow, setSelectedRow] = useState<Book | null>(null);
 
   const {
@@ -95,11 +91,10 @@ export default function App() {
   ];
 
   return (
-    <div className="flex flex-col text-gray-100 min-h-screen font-['Inter'] relative ">
-      <Hamburger open={open} setOpen={setOpen} setKind={setKind} kind={kind} />
+    <div className="flex flex-col text-gray-100 min-h-screen font-['Inter'] relative overflow-hidden">
       <Header />
-      <div className="flex flex-1 w-full h-full  max-w-[100rem] mx-auto md:p-10">
-        <div className="lg:basis-1/4 lg:sticky lg:top-10 h-full hidden lg:block">
+      <div className="flex flex-1 w-full h-full  max-w-[100rem] mx-auto md:p-10 ">
+        <div className="lg:basis-1/4 lg:sticky lg:top-10 top-0 left-0 absolute lg:h-full z-10 lg:z-0">
           <Nav kind={kind} setKind={changeKind} />
         </div>
         <div className="basis-full lg:basis-3/4 px-4 mx-auto md:px-10 flex flex-col">
--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
 import { Kind } from "../../types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 
 interface NavProps {
   kind: Kind;
@@ -6,91 +9,111 @@ interface NavProps {
 }
 
 const Nav = ({ kind, setKind }: NavProps) => {
+  const [open, setOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setOpen(!open);
+  };
+
+  const handleClick = (kind: Kind) => {
+    setKind(kind);
+    setOpen(false);
+  };
+
   return (
-    <div className="hidden lg:block">
-      <h2 className="mb-4  font-semibold text-slate-900 dark:text-slate-200">
-        Kind
-      </h2>
-      <ul className="space-y-6 lg:space-y-2 border-l border-slate-100 dark:border-slate-800">
-        <li>
-          <button
-            onClick={() => setKind("General Literature")}
-            className={`nav ${kind === "General Literature" && "selected"}`}
-          >
-            Not specified
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={() => setKind("Animals, Bugs & Pets")}
-            className={`nav ${kind === "Animals, Bugs & Pets" && "selected"}`}
-          >
-            Animals, Bugs & Pets
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={() => setKind("Art, Creativity & Music")}
-            className={`nav ${
-              kind === "Art, Creativity & Music" && "selected"
-            }`}
-          >
-            Art, Creativity & Music
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={() => setKind("Hobbies, Sports & Outdoors")}
-            className={`nav ${
-              kind === "Hobbies, Sports & Outdoors" && "selected"
-            }`}
-          >
-            Hobbies, Sports & Outdoors
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={() => setKind("Science Fiction & Fantasy")}
-            className={`nav ${
-              kind === "Science Fiction & Fantasy" && "selected"
-            }`}
-          >
-            Science Fiction & Fantasy
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={() => setKind("Real Life")}
-            className={`nav ${kind === "Real Life" && "selected"}`}
-          >
-            Real Life
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={() => setKind("Science & Technology")}
-            className={`nav ${kind === "Science & Technology" && "selected"}`}
-          >
-            Science & Technology
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={() => setKind("Mystery & Suspense")}
-            className={`nav ${kind === "Mystery & Suspense" && "selected"}`}
-          >
-            Mystery & Suspense
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={() => setKind("Reference")}
-            className={`nav ${kind === "Reference" && "selected"}`}
-          >
-            Reference
-          </button>
-        </li>
-      </ul>
+    <div className={`bg-gray-900 ${open && "fixed top-0 h-full"}`}>
+      <button
+        className="lg:hidden text-slate-900 pl-4 pt-4 dark:text-slate-200"
+        onClick={toggleMenu}
+      >
+        <FontAwesomeIcon icon={open ? faXmark : faBars} className="text-2xl" />
+      </button>
+
+      <div className={`${open ? "block" : "hidden"} lg:block p-10 lg:p-0`}>
+        <h2 className="mb-4 font-semibold text-slate-900 dark:text-slate-200">
+          Kind
+        </h2>
+        <ul className="space-y-6 lg:space-y-2 border-l border-slate-100 dark:border-slate-800">
+          <li>
+            <button
+              onClick={() => handleClick("General Literature")}
+              className={`nav ${kind === "General Literature" && "selected"}`}
+            >
+              Not specified
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={() => handleClick("Animals, Bugs & Pets")}
+              className={`nav ${kind === "Animals, Bugs & Pets" && "selected"}`}
+            >
+              Animals, Bugs & Pets
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={() => handleClick("Art, Creativity & Music")}
+              className={`nav ${
+                kind === "Art, Creativity & Music" && "selected"
+              }`}
+            >
+              Art, Creativity & Music
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={() => handleClick("Hobbies, Sports & Outdoors")}
+              className={`nav ${
+                kind === "Hobbies, Sports & Outdoors" && "selected"
+              }`}
+            >
+              Hobbies, Sports & Outdoors
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={() => handleClick("Science Fiction & Fantasy")}
+              className={`nav ${
+                kind === "Science Fiction & Fantasy" && "selected"
+              }`}
+            >
+              Science Fiction & Fantasy
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={() => handleClick("Real Life")}
+              className={`nav ${kind === "Real Life" && "selected"}`}
+            >
+              Real Life
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={() => handleClick("Science & Technology")}
+              className={`nav ${kind === "Science & Technology" && "selected"}`}
+            >
+              Science & Technology
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={() => handleClick("Mystery & Suspense")}
+              className={`nav ${kind === "Mystery & Suspense" && "selected"}`}
+            >
+              Mystery & Suspense
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={() => handleClick("Reference")}
+              className={`nav ${kind === "Reference" && "selected"}`}
+            >
+              Reference
+            </button>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
